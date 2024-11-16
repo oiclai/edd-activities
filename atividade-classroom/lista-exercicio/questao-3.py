f40e49806c5ed9e60fec192f0788f703c2b2561c
oiclai <claradealcantara285@gmail.com>
2024-11-16 18:11:28 -0300

    :books: docs: add file 'questão-4'

--- atividade-classroom/lista-exercicio/questao-3.py
+++ atividade-classroom/lista-exercicio/questao-3.py
@@ -4,3 +4,25 @@ class Country:
         self.__capital = capital
         self.__area = areaKm2
         self.__borderContries = borderContries
+    def __str__(self):
+        return f'Nome: {self.__name} | Capital: {self.__capital} | Area: {self.__area} | Contries: {self.__borderContries}'
+    def getName(self):
+        return self.__name
+    def getCapital(self):
+        return self.__capital
+    def getArea(self):
+        return self.__area
+    def getBorderContries(self):
+        return self.__borderContries
+    def addBorderContries(self, borderContries:list, borderContrie:str):
+        self.__borderContries = borderContries
+        if isinstance(borderContrie, str) and borderContrie.strip():
+            if borderContrie not in self.__borderContries:
+                self.__borderContries.append(borderContrie)
+        return self.__borderContries
+    
+# programa principal (teste)
+pais = Country('Brasil', ['Argentina', 'Paraguay', 'Uruguay'], 'Brasilia', 8.516)
+print(pais)
+pais.addBorderContries(pais.getBorderContries(), 'Colombia')
+print(pais)
