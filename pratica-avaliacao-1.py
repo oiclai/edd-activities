e5c57807822e218e8dcff5048afea7677cf7b83b
oiclai <claradealcantara285@gmail.com>
2024-12-09 10:03:23 -0300

    :sparkles: feat: pilha

--- pratica-avaliacao-1.py
+++ pratica-avaliacao-1.py
@@ -14,4 +14,22 @@ Implemente o método para checar_duplicidade da lista.
 Implemente o método para obter um elemento da posição X da lista.
 Implemente o método para alterar o valor de uma posição X da lista.
 '''
-
+class Node:
+    def __init__(self, dado, proximo=None): # proximo = opcional, None = indicando final da pilha
+        self.dado = dado
+        self.proximo = None|Node = proximo
+class Pilha:
+    def __init__(self):
+        self.topo = None
+    def empilhar(self, dado):
+        self.topo = Node(dado, self.topo)
+    def desempilhar(self):
+        dado = self.topo.dado
+        self.topo = self.topo.proximo
+        return dado
+    def inverter(self):
+        pilhaAuxiliar = Pilha()
+        while not self.estah_vazia():
+            pilhaAuxiliar.empilhar(self.desempilhar())
+        while not pilhaAuxiliar.estah_vazia():
+            self.empilhar(pilhaAuxiliar.desempilhar())
--- teste.py
+++
@@ -1,4 +0,0 @@
-import lista
-
-head = None
-L= insere_lista(head, "a")
