8d6de2bbdbc8715515eac2f2613b3473fc81c6bc
Clara de Alcântara <161838054+oiclai@users.noreply.github.com>
2025-01-24 13:20:37 -0300

    :sparkles: Update questao1.py

--- recursao/lista-iniciante/questao1.py
+++ recursao/lista-iniciante/questao1.py
@@ -3,8 +3,8 @@ Faça uma função recursiva chamada recursiveLength()
 que retorne a quantidade de caracteres de um string.
 '''
 
-def recursiveLength(palavra):
-    if len(palavra) == 0:
+def recursiveLength(palavra:str) -> int:
+    if len(palavra) == 0: # ou palavra == ""
         return 0
     return 1 + recursiveLength(palavra[1:]) # -> 'desmontando' a string 
 
